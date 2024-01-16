a29296aa9e2c812b897afa86b1b0c970b2c4d8da
popcornell <cornellsamuele@gmail.com>
2024-01-15 20:20:14 -0500

    added text normalization

---
+++ chime_utils/text_norm/c7dasr.py
@@ -0,0 +1,45 @@
+"""
+legacy CHiME-7 DASR and CHiME-6 text normalization.
+"""
+import jiwer
+from jiwer.transforms import RemoveKaldiNonWords
+from lhotse.recipes.chime6 import normalize_text_chime6
+
+jiwer_chime6_scoring = jiwer.Compose(
+    [
+        RemoveKaldiNonWords(),
+        jiwer.SubstituteRegexes({r"\"": " ", "^[ \t]+|[ \t]+$": "", r"\u2019": "'"}),
+        jiwer.RemoveEmptyStrings(),
+        jiwer.RemoveMultipleSpaces(),
+    ]
+)
+jiwer_chime7_scoring = jiwer.Compose(
+    [
+        jiwer.SubstituteRegexes(
+            {
+                "(?:^|(?<= ))(hm|hmm|mhm|mmh|mmm)(?:(?= )|$)": "hmmm",
+                "(?:^|(?<= ))(uhm|um|umm|umh|ummh)(?:(?= )|$)": "ummm",
+                "(?:^|(?<= ))(uh|uhh)(?:(?= )|$)": "uhhh",
+            }
+        ),
+        jiwer.RemoveEmptyStrings(),
+        jiwer.RemoveMultipleSpaces(),
+    ]
+)
+
+
+def chime6_norm_scoring(txt):
+    return jiwer_chime6_scoring(normalize_text_chime6(txt, normalize="kaldi"))
+
+
+def chime7_norm_scoring(txt):
+    """
+    here we also normalize non-words sounds such as hmmm which are quite a lot !
+    you are free to use whatever normalization you prefer for training but this
+    normalization below will be used when we score your submissions.
+    """
+    return jiwer_chime7_scoring(
+        jiwer_chime6_scoring(
+            normalize_text_chime6(txt, normalize="kaldi")
+        )  # noqa: E731
+    )  # noqa: E731
---
+++ chime_utils/text_norm/c8dasr.py
@@ -0,0 +1,7 @@
+"""
+CHiME-8 DASR and NOTSOFAR1 text normalization. Based on Whisper one but with
+crucial differences.
+"""
+from whisper_like.english import EnglishTextNormalizer
+
+text_normalizer = EnglishTextNormalizer()
