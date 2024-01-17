8a5893c36064e146687d1da7b9ec797016cfa114
popcornell <cornellsamuele@gmail.com>
2024-01-16 20:01:38 -0500

    made function to select normalization

---
+++ chime_utils/text_norm/__init__.py
@@ -0,0 +1,17 @@
+from chime_utils.text_norm.c8dasr import EnglishTextNormalizer
+from chime_utils.text_norm.c7dasr import chime7_norm_scoring
+from chime_utils.text_norm.c7dasr import chime6_norm_scoring
+
+
+def get_txt_norm(txt_norm):
+    assert txt_norm in ['chime6', 'chime7', 'chime8', None]
+    if txt_norm is None:
+        return None
+    elif txt_norm == 'chime8':
+        return EnglishTextNormalizer()
+    elif txt_norm == 'chime7':
+        return chime7_norm_scoring
+    elif txt_norm == 'chime6':
+        return chime6_norm_scoring
+    else:
+        raise NotImplementedError
--- chime_utils/text_norm/c8dasr.py
+++
@@ -1,7 +0,0 @@
-"""
-CHiME-8 DASR and NOTSOFAR1 text normalization. Based on Whisper one but with
-crucial differences.
-"""
-from whisper_like.english import EnglishTextNormalizer
-
-text_normalizer = EnglishTextNormalizer()
