a595fd4d59c4027c174fec0940892d46d8d3a5bf
popcornell <cornellsamuele@gmail.com>
2024-02-13 14:05:24 -0500

    updated version for notsofar

--- chime_utils/dgen/notsofar1.py
+++ chime_utils/dgen/notsofar1.py
@@ -26,10 +26,10 @@ NOTSOFAR1_FS = 16000
 def download_notsofar1(download_dir, subset_name):
     if subset_name == "dev":
         subset_name = "dev_set"
-        version = "240130.1_dev"
+        version = "240208.2_dev"
     elif subset_name == "train":
         subset_name = "train_set"
-        version = "240130.1_train"
+        version = "240208.2_train"
     else:
         raise RuntimeError("Evaluation data has not yet been released !")
     dev_meetings_dir = download_meeting_subset(
