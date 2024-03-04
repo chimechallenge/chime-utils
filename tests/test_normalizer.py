f598fef8f0af90bfc867152e042b8be83fa70497
popcornell <cornellsamuele@gmail.com>
2024-03-04 14:39:29 -0500

    changed normalization

--- tests/test_normalizer.py
+++ tests/test_normalizer.py
@@ -23,25 +23,32 @@ def test_text_normalizer(std):
         == "o one of the most common errors is"
     )
     assert std("goin") == "going"
-    assert std("uhhh") == "uh"
-    # assert std("t v") == "tv"
+    assert std("uhhh") == ""
     assert std("et voila") == "et voila"
-    assert std("huh") == "uh"
+    assert std("huh") == ""
     assert std("aha") == "aha"
     assert std("one hundred") == "one hundred"
     assert std("one") == "one"
     assert std("ohhh wow") == "oh wow"
-    assert std("ah") == "ah"
+    assert std("ah") == ""
     assert std("wi-fi") == "wifi"
     assert std("wi fi") == "wifi"
-    assert std("ummm hmmm") == "hmm hmm"
-    assert std("uhh uhm") == "uh hmm"
-    assert std("ahh okay") == "ah okay"
+    assert std("ummm hmmm") == ""
+    assert std("uhh oh") == "oh"
+    assert std("ahh okay") == "ok"
+    assert std("okay") == "ok"
+    assert std("20$") == "twenty dollars"
+    assert (
+        std(
+            "uh well hopefully the process is still going on aaron but uh in the about uh forty minutes that way and about thirty years ago i moved into the city and uh"
+        )
+        == "well hopefully the process is still going on aaron but in the about forty minutes that way and about thirty years ago i moved into the city and"
+    )
     assert (
         std("like five hundred dollars [unintelligible] per head" " [unintelligible]")
         == "like five hundred dollars per head"
     )
     assert (
         std("hmmm this is not as bad [unintelligible] ummm probably thirty" " minutes")
-        == "hmm this is not as bad hmm probably thirty minutes"
+        == "this is not as bad probably thirty minutes"
     )
